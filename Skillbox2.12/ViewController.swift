533ea72cead96d66f996215dc703b93bea5c1637
Artem Akopian <artem7498@gmail.com>
2021-02-25 17:05:02 +1000

    first try with code deleted after

--- Skillbox2.12/ViewController.swift
+++ Skillbox2.12/ViewController.swift
@@ -6,12 +6,65 @@
 //
 
 import UIKit
+import AVKit
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let captureSession = AVCaptureSession()
+//        captureSession.sessionPreset = .photo
+        
+        guard let captureDevice = AVCaptureDevice.default(for: .video) else {return}
+        guard let input = try? AVCaptureDeviceInput(device: captureDevice) else { return }
+        captureSession.addInput(input)
+        
+        captureSession.startRunning()
+        
+        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        view.layer.addSublayer(previewLayer)
+        previewLayer.frame = view.frame
+        
+        let videoDataOutput = AVCaptureVideoDataOutput()
+        videoDataOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoQueue"))
+        captureSession.addOutput(videoDataOutput)
+        
+        var faceLayers: [CAShapeLayer] = []
+        
+        
+    }
+    
+    
+    
+    
+    
+    func captureOutput(_ output: AVCaptureOutput, didDrop sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        
+        
+        guard let imageBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
+        
+        let faceDetectionRequest = VNDetectFaceLandmarksRequest { (request, error) in
+            DispatchQueue.main.async {
+                self.faceLayers
+            }
+        }
+        
+//        guard let pixelBuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else {return}
+//
+//        let request = VNDetectFaceRectanglesRequest { (req, err) in
+//            if let error = err{
+//                print("Filed to detect faces:", err)
+//                return
+//            }
+//
+//            print(req)
+//
+//        }
+//
+//        try? VNImageRequestHandler(cmSampleBuffer: pixelBuffer, options: [:]).perform([request])
     }
 
 
